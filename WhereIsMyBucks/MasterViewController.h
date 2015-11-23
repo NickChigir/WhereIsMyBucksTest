3f1cafa71f40c7431fcd9543726a1ab8319850c4
Nick Chigir <nickchigir@nicks-macbook-pro.local>
2015-11-24 00:46:43 +0200

    Initial Commit

---
+++ WhereIsMyBucks/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  WhereIsMyBucks
+//
+//  Created by Nick Chigir on 11/24/15.
+//  Copyright © 2015 Nick Chigir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
